38902f848e5473bb177328f712908edf879038d8
Jonny Takano <jtakano@eaibrasil.com>
2025-01-23 17:56:48 -0300

    initial

---
+++ src/Ambev.DeveloperEvaluation.Domain/Entities/Product.cs
@@ -0,0 +1,20 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Common;
+using Ambev.DeveloperEvaluation.Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ambev.DeveloperEvaluation.Domain.Entities
+{
+    public class Product : BaseEntity
+    {
+        public string Title { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public string Image { get; set; }
+        public Rating Rating { get; set; }
+    }
+}
---
+++ src/Ambev.DeveloperEvaluation.Domain/Repositories/IProductRepository.cs
@@ -0,0 +1,52 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ambev.DeveloperEvaluation.Domain.Repositories
+{
+    public interface IProductRepository
+    {    
+         /// <summary>
+         /// Creates a new product in the repository
+         /// </summary>
+         /// <param name="product">The product to create</param>
+         /// <param name="cancellationToken">Cancellation token</param>
+         /// <returns>The created user</returns>
+        Task<User> CreateAsync(Product product, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Update a product in the repository
+        /// </summary>
+        /// <param name="product">The product to create</param>
+        /// <param name="cancellationToken">Cancellation token</param>
+        /// <returns>The created user</returns>
+        Task<User> UpdateAsync(Product product, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Retrieves a user by their unique identifier
+        /// </summary>
+        /// <param name="id">The unique identifier of the product</param>
+        /// <param name="cancellationToken">Cancellation token</param>
+        /// <returns>The user if found, null otherwise</returns>
+        Task<User?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Retrieves a user by their email address
+        /// </summary>
+        /// <param name="email">The email address to search for</param>
+        /// <param name="cancellationToken">Cancellation token</param>
+        /// <returns>The user if found, null otherwise</returns>
+        Task<User?> GetByEmailAsync(string email, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Deletes a user from the repository
+        /// </summary>
+        /// <param name="id">The unique identifier of the user to delete</param>
+        /// <param name="cancellationToken">Cancellation token</param>
+        /// <returns>True if the user was deleted, false if not found</returns>
+        Task<bool> DeleteAsync(Guid id, CancellationToken cancellationToken = default);
+    }
+}
